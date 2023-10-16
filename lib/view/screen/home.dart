6147703d1c43b4c15ee82c08ea98aca5bf7670ed
unknown <raneem 12129th@gmail.com>
2023-10-16 09:08:47 -0700

    add othersssss

--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -121,6 +121,8 @@ class ListItemsSearch extends GetView<HomeShopeControllerImp> {
                           subtitle: Text(listdatamodel[index].categoriesName!),
                         )),
                   ],
+
+
                 ),
               )),
             ),
