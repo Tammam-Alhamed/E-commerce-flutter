42c5473f7f03d3779ffe08dfab552bcaf0a6dfbc
tammam <alhamdtmam@gmail.com>
2024-01-04 11:47:35 +0300

    fix some photo

--- lib/core/constant/imgaeasset.dart
+++ lib/core/constant/imgaeasset.dart
@@ -2,6 +2,7 @@ class AppImageAsset {
   static const String rootImages = "assets/images";
   static const String rootLottie = "assets/lottie";
   static const String logo = "$rootImages/logoapp.png";
+  static const String logoAuth = "$rootImages/logoauth.png";
   static const String onBoardingImageOne = "$rootImages/one.PNG";
   static const String onBoardingImageTwo = "$rootImages/two.PNG";
   static const String onBoardingImageThree = "$rootImages/three.PNG";
